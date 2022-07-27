c5ce524cb8d7e63c981927ef5d69d8272cf4ef3f
Daniil Shvedov <dankaprog@gmail.com>
2022-07-27 16:45:18 +0300

    add user tests

---
+++ tests/test_user.py
@@ -0,0 +1,30 @@
+import os
+import pytest
+from dotenv import load_dotenv
+from tiktok_rapidapi import TikTokRapidAPI
+from tiktok_rapidapi.schemas import TikTokUserModel
+
+load_dotenv()
+rapidapi_host = os.environ.get("rapidapi_host")
+rapidapi_key = os.environ.get("rapidapi_key")
+
+if not rapidapi_key or not rapidapi_host:
+    raise Exception("Enter credentials in env")
+
+
+@pytest.fixture
+def ttapi():
+    return TikTokRapidAPI(
+        rapidapi_host=rapidapi_host,
+        rapidapi_key=rapidapi_key
+    )
+
+
+@pytest.mark.asyncio
+async def test_get_user_data_by_username(ttapi):
+    res = await ttapi.get_user_data_by_username(username="nike")
+
+
+@pytest.mark.asyncio
+async def test_get_user_data_by_id(ttapi):
+    res = await ttapi.get_user_data_by_id(user_id='208464585232822272')
